ba8d3ceaa96800c084d18b0082b52236de368083
fabioono25 <fabioono25@gmail.com>
2023-08-11 09:45:37 +0200

    using suspense

--- 8-NextJS13/1-my-youtube-demo/app/(user)/todos/TodosLists.tsx
+++ 8-NextJS13/1-my-youtube-demo/app/(user)/todos/TodosLists.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import React from "react";
 
 const fetchTodos = async () => {
+
+  //timeout for random number of seconds between 1 and 5
+  const timeout = Math.floor(Math.random() * 5) + 1;
+  await new Promise(resolve => setTimeout(resolve, timeout * 1000));
+
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const todos: Todo[] = await res.json();
   return todos;
--- 8-NextJS13/1-my-youtube-demo/app/page.tsx
+++ 8-NextJS13/1-my-youtube-demo/app/page.tsx
@@ -1,5 +1,23 @@
 import Image from "next/image";
+import TodosList from "./(user)/todos/TodosLists";
+import { Suspense } from "react";
 
 export default function Home() {
-  return <div>I am the homepage</div>;
+  return (
+    <div>
+      <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+        <h1>Loading todos</h1>
+        <div className="flex space-x-2">
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense fallback={<p>Loading the Shopping Trolley...</p>}>
+        <h1>Loading Shopping Trolley</h1>
+        <div className="flex space-x-2">
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
 }
