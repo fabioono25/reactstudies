d8d57cb45fe81808842a7882f76d4ae4c3e94532
fabioono25 <fabioono25@gmail.com>
2023-08-11 09:25:05 +0200

    working with client and server components

---
+++ 8-NextJS13/1-my-youtube-demo/app/search/[searchTerm]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div>Loading search results. Please wait...</div>
+  )
+}
+
+export default Loading
---
+++ 8-NextJS13/1-my-youtube-demo/app/search/[searchTerm]/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+type PageProps = {
+  params: {
+    searchTerm: string;
+  };
+};
+
+type SearchResult = {
+  organic_results: [
+    {
+      position: number;
+      title: string;
+      link: string;
+      thumbnail: string;
+      snippet: string;
+    }
+  ];
+};
+
+const search = async (searchTerm: string) => {
+  const res = await fetch(
+    `https://serpapi.com/search.json?q=${searchTerm}&api_key=${process.env.API_KEY}`
+  );
+  const results = await res.json();
+  return results;
+};
+
+async function SearchResults({ params: { searchTerm } }: PageProps) {
+  const searchResults = await search(searchTerm);
+
+  return (
+    <div>
+      <p className="text-gray-500 text-sm">You searched for: {searchTerm}</p>
+
+      <ol className="space-y-5 p-5">
+        {searchResults.organic_results.map((result: any) => (
+          <li key={result.position} className="flex space-x-4">
+            <p className="font-bold">{result.title}</p>
+            <p>{result.snipped}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default SearchResults;
