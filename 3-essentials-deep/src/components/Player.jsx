8a4b9d75908c17108e816cdf0eb60d36037a0912
fabioono25 <fabioono25@gmail.com>
2026-01-09 16:53:13 +0100

    finish Max studies here

--- 3-essentials-deep/src/components/Player.jsx
+++ 3-essentials-deep/src/components/Player.jsx
@@ -1,54 +1,43 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 
-export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
-    const [isEditing, setIsEditing] = useState(false);
-    const inputRef = useRef(null);
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
 
-    useEffect(() => {
-        if (isEditing && inputRef.current) {
-            inputRef.current.focus();
-        }
-    }, [isEditing]);
-
-    function handleEditClick() {
-        setIsEditing((editing) => !editing);
-    }
-
-    function handleChange(event) {
-        setPlayerName(event.target.value);
-    }
-
-    function handleKeyDown(event) {
-        if (event.key === 'Enter') {
-            handleEditClick();
-        }
-    }
-
-    let editablePlayerName = <span className="player-name">{playerName}</span>;
-    // let btnCaption = 'Edit';
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
 
     if (isEditing) {
-        editablePlayerName = (
-            <input
-                type="text"
-                required
-                value={playerName}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                ref={inputRef}
-            />
-        );
-        // btnCaption = 'Save';
+      onChangeName(symbol, playerName);
     }
+  }
 
-    return (
-        <li>
-            <span className="player">
-                {editablePlayerName}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
-        </li>
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
+    // btnCaption = 'Save';
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  );
 }
